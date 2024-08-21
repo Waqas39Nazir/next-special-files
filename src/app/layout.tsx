aaefe764944c61171e372be7218b95d2daa8e038
Waqas Nazir <waqasnazir@altaurux.com>
2024-08-21 23:12:19 +0500

    Special Files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="min-h-screen flex flex-col">
-        <Header />
+        <Header name="Waqas - Saad" age="12" />
         <main className="flex-grow">{children}</main>
         <Footer />
       </body>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import Link from "next/link";
 
-const Header = () => {
+// type Props = {
+//   name: string | number;
+// };
+
+// type Props = {
+//     age: string | number;
+// }
+
+interface Props {
+  name: string | number;
+}
+
+interface Props {
+  age: string | number;
+}
+
+const Header = ({ name, age }: Props) => {
   return (
     <header className="bg-gray-800 p-4">
       <nav className="flex justify-between items-center">
         <div className="text-white text-xl font-bold">MyWebsite</div>
         <div className="flex space-x-4">
           <Link className="text-white hover:text-gray-300" href="/">
-            Home
+            Home {name} {age}
           </Link>
           <Link className="text-white hover:text-gray-300" href="/products">
             Products
